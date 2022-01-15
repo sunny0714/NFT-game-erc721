774cc36e8da0e04f194953749b9a166883a64bed
sunny0714 <sun2020victory@gmail.com>
2022-01-15 08:47:29 +0200

    update contract

--- contracts/SunnyToken.sol
+++ contracts/SunnyToken.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SunnyToken is ERC721Full {
-  constructor() ERC721Full("Sunny Token", "SUNONE") public {
-  }
+contract SunnyToken is ERC721URIStorage {
+  using Counters for Counters.Counter;
+
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("Sunny Token", "SUNONE") {}
 
   function mint(address _to, string memory _tokenURI) public returns(bool) {
-    uint _tokenId = totalSupply().add(1);
+    _tokenIds.increment();
+    uint256 _tokenId = _tokenIds.current();
     _mint(_to, _tokenId);
     _setTokenURI(_tokenId, _tokenURI);
     return true;
