d4cff0cd92c33a2f53ce9bd86bc32a6ee8a3f81a
sunny0714 <sun2020victory@gmail.com>
2022-01-15 03:06:16 +0200

    add smart contract

--- contracts/SunnyToken.sol
+++ contracts/SunnyToken.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SunnyToken {
-  constructor() public {
+import "./ERC721Full.sol";
+
+contract SunnyToken is ERC721Full {
+  constructor() ERC721Full("Sunny Token", "SUNONE") public {
+  }
+
+  function mint(address _to, string memory _tokenURI) public returns(bool) {
+    uint _tokenId = totalSupply().add(1);
+    _mint(_to, _tokenId);
+    _setTokenURI(_tokenId, _tokenURI);
+    return true;
   }
 }
